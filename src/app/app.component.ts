f51f39f8ae1d19bf71871e4ee8d32f5b55f1aeb5
MikeB <mikebendorf11@gmail.com>
2018-09-23 21:24:40 -0700

    router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template:   
+      `<h1>This is the header</h1>
+    <nav>
+    <a routerLink="/page-a" routerLinkActive="active">A</a> |
+    <a routerLink="/page-b/5/bob" routerLinkActive="active">B</a>
+    </nav>
+    <!-- Where router should display a view -->
+    <router-outlet></router-outlet>`
 })
-export class AppComponent {
-  title = 'myapp2';
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { PageDefault }    from './app.pagedefault';
+import { PageAComponent } from './app.page-a';
+import { PageBComponent } from './app.page-b';
+import { routing }        from './app.routing';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [BrowserModule, FormsModule, routing],
+    declarations: [AppComponent, PageDefault,
+        PageAComponent, PageBComponent],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
