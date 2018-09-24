158af01050d9a70f20b563d1b6d99c7f03c0ec63
MikeB <mikebendorf11@gmail.com>
2018-09-23 23:32:28 -0700

    another way to send data but still need to trigger event with (change) and enter

--- src/app/app.page-b.ts
+++ src/app/app.page-b.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
 @Component({
     template: `This is page b. {{retrievedFromStorage}}
-    <input type="input"  (change)="mychange($event)" >
+    <input type="input" (change)="mychange()" [(ngModel)]="asd" >
     
     `
 })
 export class PageBComponent {
     asd: string;
     constructor() {
-
+        this.asd =''
     }
-    mychange(e) {
-        sessionStorage.setItem("autosave", e.target.value);
-        this.asd = e.target.value;
-        console.log(e); // updated value
+    mychange() {
+        sessionStorage.setItem("autosave", this.asd);
+        console.log(this.asd); // updated value
     }
 }
