4f2ca4108221c4398b7c95ed460afe66b6a61c82
MikeB <mikebendorf11@gmail.com>
2018-09-24 09:29:17 -0700

    last question

--- src/app/app.page-a.ts
+++ src/app/app.page-a.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 @Component({
-    template: `This is page A.<br>This content was set in page-b: {{someItem}}`
+    template: `This is page A.
+    <br>This content was set in page-b: {{someItem}}
+    <br>AND This content was set in page-b: {{someItem2}}`
 })
 export class PageAComponent {
     someItem: string;
+    someItem2: string;
     
 
     constructor() {
         if (sessionStorage.getItem("autosave")) {
             this.someItem = sessionStorage.getItem("autosave");
+        }
+        if (sessionStorage.getItem("autosave2")) {
+            this.someItem2 = sessionStorage.getItem("autosave2");
         }
     }
   }
--- src/app/app.page-b.ts
+++ src/app/app.page-b.ts
@@ -2,16 +2,22 @@ import { Component } from '@angular/core';
 @Component({
     template: `This is page b. {{retrievedFromStorage}}
     <input type="input" (change)="mychange()" [(ngModel)]="asd" >
-    
+    <input type="input" (change)="mychange2()" [(ngModel)]="asd2" >
     `
 })
 export class PageBComponent {
     asd: string;
+    asd2: string;
     constructor() {
         this.asd =''
+        this.asd2 =''
     }
     mychange() {
         sessionStorage.setItem("autosave", this.asd);
         console.log(this.asd); // updated value
+    }
+    mychange2() {
+        sessionStorage.setItem("autosave2", this.asd2);
+        console.log(this.asd2); // updated value
     }
 }
