72991336e53c2bd8c3719e3c5b1b336ca4d1b609
MikeB <mikebendorf11@gmail.com>
2018-10-02 13:30:58 -0700

    Master clean, no directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NewDirective } from './newDirective';
-import { NewDirective3 } from './thirdDirective';
+
 
 @NgModule({
   declarations: [
-    AppComponent, NewDirective, NewDirective3
+    AppComponent, 
   ],
   imports: [
     BrowserModule
