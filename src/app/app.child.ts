1d4ea3f52d0b33de0bcc72d86043150c1e412c9a
MikeB <mikebendorf11@gmail.com>
2018-10-02 16:25:56 -0700

    2 callbacks, one child, with error

--- src/app/app.child.ts
+++ src/app/app.child.ts
@@ -3,17 +3,27 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'child',
     template: `<input type="submit" value="{{opType}}"    
-                     (click)="callParent()"/>
-                `
+                (click)="callParent()"/>
+                <input type="button" value="{{numbers}}"    
+                (click)="callParent2()"/>`
 })
 export class ChildComponent {
     @Input()
+    numbers: string;
+
+    @Input()
     public childFuncRef: Function;
+
+    @Input()
+    public childFuncRef2: Function;
 
     @Input() 
     public opType:string;
 
     callParent() {
         this.childFuncRef(this.opType);
+    }
+    callParent2() {
+      this.childFuncRef2(this.numbers);
     }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,28 @@ import { ChildComponent } from './app.child';
   selector: 'app-root',
   template: `
   <div *ngFor="let op of operations">
-  <child [childFuncRef]="parentFuncRef" [opType]="op"></child>
+  <child [childFuncRef]="parentFuncRef" [childFuncRef2]="parentFuncRef2" [opType]="op" [numbers]="this.generateNumber()"></child>
   </div>`
 })
 export class AppComponent {
   public parentFuncRef: Function;
+  public parentFuncRef2: Function;
   public operations:Array<any>;
 
   public ngOnInit() {
     // Create a reference to function within this component.
     this.parentFuncRef = this.myCallBackFunction.bind(this);
+    this.parentFuncRef2 = this.secondCallbackFunction.bind(this);
     this.operations = ['+','-','*', '/'];
   }
   // This function can be called by child.
   public myCallBackFunction(operation) {
     alert("Received from child: " + operation);
+  }
+  public secondCallbackFunction(number) {
+    alert("Received from child: " + number);
+  }
+  generateNumber(){
+    return Math.random();    
   }
 }
