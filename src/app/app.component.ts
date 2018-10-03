98030a588daadd256e9976c3093f5be9e8a5b3c2
MikeB <mikebendorf11@gmail.com>
2018-10-02 17:33:24 -0700

    Events 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ViewFlags } from '@angular/compiler/src/core';
 
 @Component({
-    selector: 'app-root',
-    template: `<h1>Hello world!  {{title}}</h1>`
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css']
 })
 export class AppComponent {
-    public title = 'This is Angular!';
-    public author = "Joyce.";
+  title = 'app';
+
+  myKeyDown(event) {  
+    console.log(event)
+    console.log(event.key)
+  }
+  myKeyUp(e){
+      console.log('up');
+      console.log(e);
+      console.log(e.key);
+  }
 }
